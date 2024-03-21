ddafbf57534ab2e7202da6dbccca25e152af800e
Daniel Urra <danielurra75@gmail.com>
2024-03-21 05:01:35 -0500

    Update JournalEntriesListView.swift

--- JournalEntriesListView.swift
+++ JournalEntriesListView.swift
@@ -1,16 +1,7 @@
-//
-//  ContentView.swift
-//  JournalEntries
-//
-//  Created by Eusebio Taba on 3/20/24.
-//
-
 import SwiftUI
 
 struct JournalEntriesListView: View {
-    
-    
-    
+      
     var body: some View {
         NavigationStack {
             List (plural){ iterateArrayMembers in
